92d22535375139c820df415cc129db537388e29e
Vlad Filchagin <gelenbagen@gmail.com>
2020-01-20 15:27:21 +0300

    added a visibility class for animations and deleted this property from blocks

--- source/js/animation.js
+++ source/js/animation.js
@@ -2,25 +2,30 @@ window.addEventListener('load', () => {
 	const typeOfSigns = document.querySelectorAll('.priceSection .container .priceSection__typesOfSigns .typeOfSign')
 	const advantages = document.querySelectorAll('.whyUsSection .container .whyUsSection__advantageList .advantage')
 	const slideInUp = 'slideInUp'
+	const slideInUpVisibility = 'slideInUp_hidden'
 
 	const maintTitles = document.querySelectorAll('.mainTitle')
 	const slideInLeft = 'slideInLeft'
+	const slideInLeftVisibility = 'slideInLeft_hidden'
 
 	function watchAnimateScroll() {
 		for (let i = 0; i < typeOfSigns.length; i++) {
 			if (window.pageYOffset >= typeOfSigns[i].getBoundingClientRect().y - this.window.innerHeight + typeOfSigns[i].getBoundingClientRect().height + this.pageYOffset) {
+				typeOfSigns[i].classList.remove(slideInUpVisibility)
 				typeOfSigns[i].classList.add(slideInUp)
 			}
 		}
 
 		for (let i = 0; i < advantages.length; i++) {
 			if (window.pageYOffset >= advantages[i].getBoundingClientRect().y - this.window.innerHeight + advantages[i].getBoundingClientRect().height + this.pageYOffset) {
+				advantages[i].classList.remove(slideInUpVisibility)
 				advantages[i].classList.add(slideInUp)
 			}
 		}
 
 		for (let i = 0; i < maintTitles.length; i++) {
 			if (window.pageYOffset >= maintTitles[i].getBoundingClientRect().y - this.window.innerHeight + maintTitles[i].getBoundingClientRect().height + this.pageYOffset) {
+				maintTitles[i].classList.remove(slideInLeftVisibility)
 				maintTitles[i].classList.add(slideInLeft)
 			}
 		}
