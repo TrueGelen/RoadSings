03ac9ba551dabe001eaf0c0a6358c8e16c5f1d2d
Vlad Filchagin <gelenbagen@gmail.com>
2020-01-20 17:04:22 +0300

    fixed bug with parallax

--- source/js/parallax.js
+++ source/js/parallax.js
@@ -1,18 +1,37 @@
 import Rellax from 'rellax'
 
 window.addEventListener('load', () => {
-	const content = new Rellax('.sizeSection__content', {
-		speed: 4,
-		center: true
-	})
+	let content = null
+	let background = null
 
-	const background = new Rellax('.parallax__background', {
-		speed: 1.6,
-		center: true
-	})
+	if (window.innerWidth > 500) {
+		content = new Rellax('.sizeSection__content', {
+			speed: (window.innerWidth > 800) ? 3 : -1,
+			center: true
+		})
+
+		background = new Rellax('.parallax__background', {
+			speed: (window.innerWidth > 768) ? 1.6 : 1,
+			center: true
+		})
+	}
 
 	window.addEventListener('resize', () => {
-		content.refresh()
-		background.refresh()
+		if (content !== null) {
+			content.destroy()
+			background.destroy()
+		}
+
+		if (window.innerWidth > 500) {
+			content = new Rellax('.sizeSection__content', {
+				speed: (window.innerWidth > 800) ? 2.8 : -1,
+				center: true
+			})
+
+			background = new Rellax('.parallax__background', {
+				speed: (window.innerWidth > 800) ? 1.6 : 1,
+				center: true
+			})
+		}
 	})
 })
